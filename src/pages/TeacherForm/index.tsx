05d297bf9e135e105ee14777f6edd4ed4e34bf57
arthurmdros <arthurmedeiros2005@gmail.com>
2020-08-06 00:36:49 -0300

    Estruturando formulário da página TeacherForm

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import Input from '../../components/Input';
 import PagHeader from '../../components/PagHeader';
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
 
 function TeacherForm(){
     return(
         <div id="page-teacher-form" className="container">
-            <PagHeader title="Que incrível que você quer dar aulas."/>
+            <PagHeader 
+                title="Que incrível que você quer dar aulas."
+                description="O primeiro passo é preencher esse formulário de inscrição."
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus Dados</legend>
+
+                    <Input name="name" label="Nome completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="WhatsApp" />
+
+                </fieldset>
+
+                
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Input name="subject" label="Matéria"/>
+                    <Input name="cost" label="Custo da sua hora por aula" />
+
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante"/>
+                        Importante! <br />
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar cadastro
+                    </button>
+                </footer>
+            </main>
         </div>
+        
     )
 }
 
