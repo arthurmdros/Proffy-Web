800f327863833a9984e06a383c3e7fd3e452ea7d
arthurmdros <arthurmedeiros2005@gmail.com>
2020-08-06 13:45:31 -0300

    Adicionando interface e inserindo valores default

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,27 +4,37 @@ import whatsAppIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
 
-function TeacherItem(){
+export interface Teacher {
+    id: number,
+    avatar: string,        
+    bio: string,        
+    cost: number,        
+    name: string,
+    subject: string,
+    whatsapp: string,  
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQchnk5how5ihE9M2MDN9qN6rv1FhP0vTvIwg&usqp=CAU" alt="Jiraya, O sábio tarado"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Jiraya, o sábio tarado</strong>
-                    <span>Putaria</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta das melhores técninas em putaria avançada.
-                <br /> <br />
-                Apaixonado por farra, putaria e mulheres. Quiser trocar ideia passa zap clicando no botão abaixo.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ Sexo, drogas e álcool</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
                 <button type="button">
                     <img src={whatsAppIcon} alt="whatsapp"/>
